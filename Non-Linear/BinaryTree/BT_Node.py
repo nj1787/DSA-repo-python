059440175a89323c8fd7c0106bf94652a27d4495
Neeraj Jain <neerajj9@gmail.com>
2022-10-27 00:14:50 +0530

    Create BT_Node.py
    Simple class of a Binary Tree node.

---
+++ Non-Linear/BinaryTree/BT_Node.py
@@ -0,0 +1,6 @@
+#Following is the blueprint of a Binary Tree node.
+class Binary_Tree_Node:
+  def __init__(self, data):  #Constructor
+    self.data = data    #Data which is passed as input while object creation. Let's assume data to be any of these types--> (String, Integer, Float) 
+    self.left = None    #
+    self.right = None   #
