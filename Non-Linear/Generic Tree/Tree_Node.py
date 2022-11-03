99071c16939aae2aa12eae02284bddbfc9355b02
Neeraj Jain <neerajj9@gmail.com>
2022-11-03 21:58:49 +0530

    Created a Generic Tree Node
    Successfully created a Generic Tree node class file.

---
+++ Non-Linear/Generic Tree/Tree_Node.py
@@ -0,0 +1,3 @@
+#Following is a general node class of a Generic Tree.
+#In Generic Tree, each node can have any number of child nodes. For that reason we use a List as one of the attributes of the node class.
+
