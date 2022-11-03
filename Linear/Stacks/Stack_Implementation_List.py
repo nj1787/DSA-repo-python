e07f4fba87936c3fe999a41218f795a624052d6e
Neeraj Jain <neerajj9@gmail.com>
2022-11-03 21:53:14 +0530

    Stack Data Structure Using Lists.
    Implemented Stacks using in built Python Lists.

---
+++ Linear/Stacks/Stack_Implementation_List.py
@@ -0,0 +1,2 @@
+#Following is The Complete Implementation of Stack Data Structure using in built Python Lists.
+#A Python List by default can be used as a stack beacuse of it's pop() and append() methods.
