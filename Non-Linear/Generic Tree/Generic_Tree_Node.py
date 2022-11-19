41e74a559122c5311635cdb0b6cef672fa15bf85
Neeraj Jain <neerajj9@gmail.com>
2022-11-19 13:51:01 +0530

    Update and rename Tree_Node.py to Generic_Tree_Node.py
    Completed Node class of Generic Tree.

--- Non-Linear/Generic Tree/Generic_Tree_Node.py
+++ Non-Linear/Generic Tree/Generic_Tree_Node.py
@@ -1,3 +1,8 @@
 #Following is a general node class of a Generic Tree.
 #In Generic Tree, each node can have any number of child nodes. For that reason we use a List as one of the attributes of the node class.
 
+class Generic_Tree_Node:
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+ 
